5be7b914e6e9d07871f1716b59a56ad4b2d96374
Matheus Ramos <matheus.hf.ramos@gmail.com>
2017-08-04 11:05:21 +0200

    started structure

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Der Die Das</title>
+</head>
+<body>
+	<header>
+		<nav id="game">
+			<ul class="options">
+				<li>Help</li>
+				<li>Config</li>
+			</ul>
+		</nav>
+		<div class="status">
+			<ul>
+				<li>Acertos</li>
+				<li>Erros</li>
+				<li>10 Palavras de 20</li>
+			</ul>
+		</div>
+	</header>
+	<main>
+		<section id="words">
+			<div class="status">Certo/ Errado</div>
+			<div class="main">Lehrer</div>
+			<div class="info">INfo</div>
+			<div class="translate">O Professor</div>
+		</section>
+		<nav id="buttons">
+			<button id="der">Der</button>
+			<button id="der">Die</button>
+			<button id="das">Das</button>
+		</nav>
+	</main>
+</body>
+</html>
