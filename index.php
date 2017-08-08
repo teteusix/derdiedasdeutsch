d28aa19020c094df333feaa6ddebd56975d8c8d3
Matheus Ramos <matheus.hf.ramos@gmail.com>
2017-08-08 09:06:42 +0200

    corrected structure html

--- index.php
+++ index.php
@@ -8,33 +8,21 @@
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Raleway:300,400,500,600,700|Work+Sans:300,400,700" rel="stylesheet">
 </head>
 <body>
-	<header id="header">
-		<div class="status">
-			<ul>
-				<!-- <li><span id="totalwords_1"></span> Palavras de <span id="totalwords_2"></span></li> -->
-			</ul>
-
-			<div class="result">
-				<p>Score</p>
-				<input type="text" id="score" name="score" value="0" disabled>
-			</div>
-		</div>
+	<header class="game-screen">
+		<h1><span>Der</span><span>Die</span><span>Das</span></h1>
 	</header>
 	<main>
 		<section id="words">
-			<div class="wp-result"></div>
-			<div id="word-platz"></div>
-			<ul>
-				<li><span><!-- <i class="fa fa-info-circle" aria-hidden="true"></i> --></span><div class="wp-info"></div></li>
-				<li><span>Pl.</span><div class="wp-plural"></div></li>
-				<li><span><i class="fa fa-language" aria-hidden="true"></i></span><div class="wp-translate"></div></li>
+			<h2 id="word-singular"></h2>
+			<ul class="word-datas">
+				<li class="word-info"><span><i class="fa fa-info-circle" aria-hidden="true"></i></span><span id="word-info">Um morador de Wein</span></li>
+				<li class="word-plural"><span>Pl.</span><span id="word-plural">Plural</span></li>
+				<li class="word-translate"><span><i class="fa fa-language" aria-hidden="true"></i></span><span id="word-translate">Weinero</span></li>
 			</ul>
 		</section>
-		<?php include 'wordlist.php'; ?>
-		<nav id="buttons"><button class="button" id="der" value="der">Der</button><button class="button" id="die" value="die">Die</button><button class="button" id="das" value="das">Das</button></nav>
 	</main>
+	<footer class="game-screen"><nav id="answer-buttons"><button class="answer-button" id="der" value="der">Der</button><button class="answer-button" id="die" value="die">Die</button><button class="answer-button" id="das" value="das">Das</button></nav></footer>
 
-	<!-- <script src="app/app.js"></script> -->
-	<script src="app/list-words.js"></script>
+	<script src="app.js"></script>
 </body>
 </html>
