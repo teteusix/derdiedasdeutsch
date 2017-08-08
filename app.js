d135eca4bc4dbddc712ac49caff7aeb08e38b4dc
Matheus Ramos <matheus.hf.ramos@gmail.com>
2017-08-08 09:57:16 +0200

    started var to recevie words and class

---
+++ app.js
@@ -0,0 +1,7 @@
+// take datas of the word
+var word_article = document.getElementById('article'),
+  word_singular = document.getElementById('singular'),
+  word_plural = document.getElementById('plural'),
+  word_information = document.getElementById('information'),
+  word_translation = document.getElementById('translation');
+
---
+++ wordlist.js
@@ -0,0 +1,10 @@
+var words = [], // array to receive word list
+	// create objects to store the words
+	word_001 = {},
+	word_002 = {},
+	word_003 = {},
+	word_004 = {},
+	word_005 = {},
+	word_006 = {};
+
+	// create key-value attributes on each object
