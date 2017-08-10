3dd3096e342a9361a03b55992f0588ca52932030
Matheus Ramos <matheus.hf.ramos@gmail.com>
2017-08-10 21:48:01 +0200

    created a sample array and generated a random word

--- app.js
+++ app.js
@@ -2,13 +2,34 @@
 var score_val = 0;
 document.getElementById('result').innerHTML = score_val;
 
+
+// ****************************************************
+
+// var wordlistJSON = '';
+
+var wordlistJSON = '{ "word" : [' +
+'{ "article":"der" , "singular": "Beruf","plural": "Die Berufe"},' +
+'{ "article":"die" , "singular": "Zahl","plural": "Die Zahlen"},' +
+'{ "article": "die" , "singular": "Banane","plural": "Die Bananen"},' +
+'{ "article": "der" , "singular": "Wein","plural": "Die Weine"},' +
+'{ "article": "das" , "singular": "Glas","plural": "Die Gläser"},' +
+'{ "article":"das" , "singular": "Haus","plural": "Die Häuser"} ]}';
+
+var wordlist = JSON.parse(wordlistJSON);
+// word_singular.innerHTML = wordlist.word[0].singular;
+// word_plural.innerHTML = wordlist.word[0].plural;
+
+console.log(wordlistJSON.length);
+console.log(wordlist);
+console.log(wordlist.word.length);
+
+
 // take datas of the word
 var word_article = document.getElementById('word-article'),
 	word_singular = document.getElementById('word-singular'),
 	word_plural = document.getElementById('word-plural'),
 	word_information = document.getElementById('word-info'),
-	word_translate = document.getElementById('word-translate'),
-	wordList = words;
+	word_translate = document.getElementById('word-translate');
 
 // Buttons
 var btn_der = document.getElementById('der'),
@@ -18,97 +39,33 @@ var btn_der = document.getElementById('der'),
 	// btn_play = document.getElementById('play');
 	// btn_play = document.getElementById('play').setAttribute('onclick','firstTurn()');
 
-var word;
-var random_word;
-// function test() {
-// console.log(words.length);
-// console.log(words);
-// var random_word = Math.floor(Math.random()*words.length);
-// console.log(random_word);
-// var word = words[random_word];
-// console.log(word);
-// words.splice(random_word,1);
-// console.log(words.length);
-// console.log(words);
-// }
+function generate_random_word () {
+	 random_word = Math.floor(Math.random()*wordlist.word.length);
+	 console.log(random_word);
 
-function generate_random_word (word,random_word) {
-	console.log('Gera palavra 1 aleatoria de '+words.length+' palavras');
-	console.log(words);
-	if (words.length > 0) {
-		// var word = words[Math.floor(Math.random()*words.length)]; //generate a random word basead in the length of array
-		li_wordInfo = document.getElementsByClassName('word-info')[0];
-		random_word = Math.floor(Math.random()*words.length);
-		console.log(random_word);
-		word = words[random_word];
-		//insert the word and data in html
-		word_article.innerHTML = word.article;
-		word_singular.innerHTML = word.singular;
-		word_plural.innerHTML = word.plural;
-		word_translate.innerHTML = word.translate;
-		if (word.information == '') {
-			li_wordInfo.style.display = 'none';
-		} else {
-			word_information.innerHTML = word.information;
-		}
-		console.log('======>  '+word.article+' '+word.singular+'  <======');
-		words.splice(random_word,1);
-		changeArray(word, random_word);
-	} else {
-		console.log('Cabo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-	}
-	console.log('======>  '+word.article+' '+word.singular+'  <======');
-	return word;
+
+	 return random_word;
 }
+generate_random_word();
 
-// function changeArray(word, words, usedWords) {
-function changeArray(word,random_word) {
+function changeArray() {
 	console.log('Muda a palavra de array');
-	// console.log(word);
-	// console.log(usedWords.length);
-	// console.log(usedWords);
-	// console.log(usedWords.length);
-	// console.log(usedWords);
-
-	console.log('removeu');
-	// words.splice(random_word,1);
-	// removediabo = words.splice(random_word,1);
-	// words = removediabo;
-	console.log(words);
-	console.log(word);
-	console.log('Enviou a palavra');
-	// usedWords.push(words[random_word]);
-	console.log(words[word]);
-	console.log(words[random_word]);
-	usedWords.push(words[random_word]);
 }
 
-function turn(word) {
+function turn() {
 	console.log('Função: Turno');
-	generate_random_word();
-	// changeArray(word);
 }
-function checkButtons() {
-	console.log('Função: Confere botões');
-}
-function nextTurn(word) {
+function checkButtons() {}
+function nextTurn() {
 	console.log('Função: Proximo Turno');
-	// console.log('Ainda não removeu');
-	// console.log(words);
-	// words.splice(word,1);
-	// console.log('removeu');
-	// console.log(words);
-
-	// console.log(usedWords.length);
-	// console.log(words.length);
-	// console.log(word);
-	turn();
 }
 function endGame() {
 	console.log('Função: Fim do jogo');
 }
 
 turn();
-// checkButtons();
-// nextTurn();
-// endGame();
+checkButtons();
+nextTurn();
+endGame();
+
+
