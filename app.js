c677fbd1189e5908d72a890a4915747e9d96257a
Matheus Ramos <matheus.hf.ramos@gmail.com>
2017-08-10 20:58:40 +0200

    try with other way but not work

--- app.js
+++ app.js
@@ -4,46 +4,111 @@ document.getElementById('result').innerHTML = score_val;
 
 // take datas of the word
 var word_article = document.getElementById('word-article'),
-  word_singular = document.getElementById('word-singular'),
-  word_plural = document.getElementById('word-plural'),
-  word_information = document.getElementById('word-info'),
-  word_translate = document.getElementById('word-translate'),
-  wordList = words;
+	word_singular = document.getElementById('word-singular'),
+	word_plural = document.getElementById('word-plural'),
+	word_information = document.getElementById('word-info'),
+	word_translate = document.getElementById('word-translate'),
+	wordList = words;
 
 // Buttons
 var btn_der = document.getElementById('der'),
-  btn_die = document.getElementById('die'),
-  btn_das = document.getElementById('das'),
-  answer_buttons = document.getElementsByClassName('answer-button'),
-  btn_play = document.getElementById('play');
-  // btn_play = document.getElementById('play').setAttribute('onclick','firstTurn()');
+	btn_die = document.getElementById('die'),
+	btn_das = document.getElementById('das'),
+	answer_buttons = document.getElementsByClassName('answer-button');
+	// btn_play = document.getElementById('play');
+	// btn_play = document.getElementById('play').setAttribute('onclick','firstTurn()');
 
-console.log(words);
+var word;
+var random_word;
+// function test() {
+// console.log(words.length);
+// console.log(words);
+// var random_word = Math.floor(Math.random()*words.length);
+// console.log(random_word);
+// var word = words[random_word];
+// console.log(word);
+// words.splice(random_word,1);
+// console.log(words.length);
+// console.log(words);
+// }
 
-var generate_random_word = function () {
-  var random_word = words[Math.floor(Math.random()*words.length)]; //generate a random word basead in the length of array
-  console.log(random_word.article +' '+random_word.singular);
-  console.log(words.length);
+function generate_random_word (word,random_word) {
+	console.log('Gera palavra 1 aleatoria de '+words.length+' palavras');
+	console.log(words);
+	if (words.length > 0) {
+		// var word = words[Math.floor(Math.random()*words.length)]; //generate a random word basead in the length of array
+		li_wordInfo = document.getElementsByClassName('word-info')[0];
+		random_word = Math.floor(Math.random()*words.length);
+		console.log(random_word);
+		word = words[random_word];
+		//insert the word and data in html
+		word_article.innerHTML = word.article;
+		word_singular.innerHTML = word.singular;
+		word_plural.innerHTML = word.plural;
+		word_translate.innerHTML = word.translate;
+		if (word.information == '') {
+			li_wordInfo.style.display = 'none';
+		} else {
+			word_information.innerHTML = word.information;
+		}
+		console.log('======>  '+word.article+' '+word.singular+'  <======');
+		words.splice(random_word,1);
+		changeArray(word, random_word);
+	} else {
+		console.log('Cabo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+	}
+	console.log('======>  '+word.article+' '+word.singular+'  <======');
+	return word;
 }
 
+// function changeArray(word, words, usedWords) {
+function changeArray(word,random_word) {
+	console.log('Muda a palavra de array');
+	// console.log(word);
+	// console.log(usedWords.length);
+	// console.log(usedWords);
+	// console.log(usedWords.length);
+	// console.log(usedWords);
 
-function generate_random_word() {
-  console.log('Função: Gera palavra');
+	console.log('removeu');
+	// words.splice(random_word,1);
+	// removediabo = words.splice(random_word,1);
+	// words = removediabo;
+	console.log(words);
+	console.log(word);
+	console.log('Enviou a palavra');
+	// usedWords.push(words[random_word]);
+	console.log(words[word]);
+	console.log(words[random_word]);
+	usedWords.push(words[random_word]);
 }
-function turn() {
-  console.log('Função: Turno');
+
+function turn(word) {
+	console.log('Função: Turno');
+	generate_random_word();
+	// changeArray(word);
 }
 function checkButtons() {
-  console.log('Função: Confere botões');
+	console.log('Função: Confere botões');
 }
-function nextTurn() {
-  console.log('Função: Proximo Turno');
+function nextTurn(word) {
+	console.log('Função: Proximo Turno');
+	// console.log('Ainda não removeu');
+	// console.log(words);
+	// words.splice(word,1);
+	// console.log('removeu');
+	// console.log(words);
+
+	// console.log(usedWords.length);
+	// console.log(words.length);
+	// console.log(word);
+	turn();
 }
 function endGame() {
-  console.log('Função: Fim do jogo');
+	console.log('Função: Fim do jogo');
 }
-generate_random_word();
+
 turn();
-checkButtons();
-nextTurn();
-endGame();
+// checkButtons();
+// nextTurn();
+// endGame();
--- wordlist.js
+++ wordlist.js
@@ -12,7 +12,7 @@ var words = [], // array to receive word list
 	word_001.article = 'der';
 	word_001.singular = 'Beruf';
 	word_001.plural = 'Die Berufe';
-	word_001.information = 'Info';
+	word_001.information = '';
 	word_001.translate = 'A profissão';
 	word_001.nivel = 'A1';
 	words.push(word_001); // insert object in the array
@@ -20,7 +20,7 @@ var words = [], // array to receive word list
 	word_002.article = 'die';
 	word_002.singular = 'Zahl';
 	word_002.plural = 'Die Zahlen';
-	word_002.information = 'info';
+	word_002.information = '';
 	word_002.translate = 'O valor';
 	word_002.nivel = 'A1';
 	words.push(word_002); // insert object in the array
@@ -28,7 +28,7 @@ var words = [], // array to receive word list
 	word_003.article = 'das';
 	word_003.singular = 'Haus';
 	word_003.plural = 'Die Häuser';
-	word_003.information = 'info';
+	word_003.information = '';
 	word_003.translate = 'A casa';
 	word_003.nivel = 'A1';
 	words.push(word_003); // insert object in the array
@@ -36,7 +36,7 @@ var words = [], // array to receive word list
 	word_004.article = 'die';
 	word_004.singular = 'Banane';
 	word_004.plural = 'Die Bananen';
-	word_004.information = 'info';
+	word_004.information = '';
 	word_004.translate = 'A Banana';
 	word_004.nivel = 'A1';
 	words.push(word_004); // insert object in the array
@@ -44,7 +44,7 @@ var words = [], // array to receive word list
 	word_005.article = 'der';
 	word_005.singular = 'Wein';
 	word_005.plural = 'Die Weine';
-	word_005.information = 'info';
+	word_005.information = '';
 	word_005.translate = 'O Vinho';
 	word_005.nivel = 'A1';
 	words.push(word_005); // insert object in the array
@@ -52,7 +52,7 @@ var words = [], // array to receive word list
 	word_006.article = 'das';
 	word_006.singular = 'Glas';
 	word_006.plural = 'Die Gläser';
-	word_006.information = 'info';
+	word_006.information = '';
 	word_006.translate = 'O copo';
 	word_006.nivel = 'A1';
 	words.push(word_006); // insert object in the array
